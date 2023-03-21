7f836ec561bca52e0483c37aecda7850d1cc5cd4
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-21 18:38:10 +0600

    use state term start and key set

--- react-Start/src/App.jsx
+++ react-Start/src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 
 function App() {
   const products=[
-    {name:'laptop',price:203333},
-    {name:'phone',price:40000},
-    {name:'wath',price:50000},
-    {name:'tablet',price:9000}
+    {name:'laptop',price:203333, id:1},
+    {name:'phone',price:40000 ,id:2},
+    {name:'wath',price:50000 ,id:3},
+    {name:'tablet',price:9000,id:4}
   ]
 	return (
 		<div >
-			{/* {products.map(({name,price})=><Product name={name} price={price}/>)} */}
+			{ products.map(({name,price,id})=><Product key={id} name={name} price={price}/>)}
 		</div>
 	);
 }
