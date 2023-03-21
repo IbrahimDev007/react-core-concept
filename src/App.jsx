f5af7526cccbe6d781af14f60cb166394cb3e67a
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-21 12:23:27 +0600

    props destrucre and dynamic component crate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
-  return (
-   <div className="App">
-    Hi
-   </div>
-  )
+  const products=[
+    {name:'laptop',price:203333},
+    {name:'phone',price:40000},
+    {name:'wath',price:50000},
+    {name:'tablet',price:9000}
+  ]
+	return (
+		<div >
+			{products.map(({name,price})=><Product name={name} price={price}/>)}
+		</div>
+	);
 }
 
-export default App
+const Product = ({name,price}) => {
+	return (
+		<div className ="product">
+			<h3>Name:{name}</h3>
+			<p>Price:{price}</p>
+		</div>
+	);
+};
+
+export default App;
